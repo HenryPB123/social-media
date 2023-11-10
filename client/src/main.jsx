ac4ac29a4c39330446e8ab07f8b1c59285ac5ebf
Henry P B <mahe2di@gmail.com>
2023-11-09 19:59:39 -0500

    Change 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { AuthContextProvider } from "./context/authContext";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
