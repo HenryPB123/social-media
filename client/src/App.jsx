719f5d1a3bdff961083ced253e13825a797fc883
Henry P B <mahe2di@gmail.com>
2023-11-10 20:30:41 -0500

    Desing login and register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Login from "./pagess/login/Login";
+import Register from "./pagess/register/Register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  return <div>Social</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
