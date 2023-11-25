fa6149e36e6ccbe70e87a50dec09a4a3906e9dc6
Henry P B <mahe2di@gmail.com>
2023-11-24 21:49:36 -0500

    creating connection db

---
+++ api/controllers/authControl.js
@@ -0,0 +1,38 @@
+import { request } from "express";
+import bcrypt from "bcryptjs";
+import { db } from "../connect.js";
+
+export const register = (req, res) => {
+  //SHECK user if exist
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (error, data) => {
+    console.log("eeeeeeeeeee", error);
+    if (error) return res.status(500).json(error);
+    if (data.length) return res.status(409).json("User already exist!");
+
+    //CREATE A NEW USER
+    //Hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const q =
+      "INSERT INTO users (`username`, `email`, `password`, `name`) VALUE (?)";
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+
+    db.query(q, values, (error, data) => {
+      console.log("eeeeeeeeeee", error);
+      if (error) return res.status(500).json(error);
+      return res.status(200).json("User has been created!");
+    });
+  });
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+
+const app = express();
+const PORT = 8800;
+
+//midlewares
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
