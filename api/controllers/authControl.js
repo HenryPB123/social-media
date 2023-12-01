6c2bdc3815094239850553687c674eaa0175ba65
Henry P B <mahe2di@gmail.com>
2023-11-30 21:34:01 -0500

    login and logout

--- api/controllers/authControl.js
+++ api/controllers/authControl.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //SHECK user if exist
@@ -31,6 +32,42 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+//!---------------------------------------------------------------------
+export const login = (req, res) => {
+  const q = "SELECT * FROM users WHERE USERNAME = ?";
 
-export const logout = (req, res) => {};
+  db.query(q, [req.body.username], (error, data) => {
+    if (error) return res.status(500).json(error);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!checkPassword)
+      return res.status(400).json("Wrong username or Password!");
+
+    const token = jwt.sign({ id: data[0].id }, process.env.SECRETKEY);
+
+    const { password, ...others } = data[0];
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  });
+};
+
+//!-----------------------------------------------------------
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out!");
+};
--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
@@ -8,10 +11,12 @@ import likeRoutes from "./routes/likes.js";
 
 const app = express();
 const PORT = 8800;
+dotenv.config();
 
 //midlewares
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
